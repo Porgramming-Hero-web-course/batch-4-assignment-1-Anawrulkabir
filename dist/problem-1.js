23418b02cf453824d0e95187eb954c906e7fa1c5
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-11-07 15:00:07 +0600

    problem-1.js file added

--- dist/problem-1.js
+++ dist/problem-1.js
@@ -1,1 +1,13 @@
 "use strict";
+// Problem 1 : Write a TypeScript function sumArray that takes an array of numbers and returns the sum of all elements in the array.
+{
+    const sumArray = (array) => {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    };
+    const result = sumArray([1, 2, 3, 4, 5]);
+    console.log(result);
+}

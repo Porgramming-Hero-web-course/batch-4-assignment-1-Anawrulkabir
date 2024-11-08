d41860ead69af6b6c933299da09e8346e0d421e2
Anawrul Kabir Fahad <100487781+Anawrulkabir@users.noreply.github.com>
2024-11-08 12:34:26 +0600

    Update problem-4.ts

--- problem-4.ts
+++ problem-4.ts
@@ -1,4 +1,4 @@
-// Problem : Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
+// Problem - 4 : Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
 
 {
   type Circle = {

0255c04250db4256acf98b06c7255277feea409c
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-11-08 11:37:18 +0600

    Problem-8 solved

--- dist/problem-8.js
+++ dist/problem-8.js
@@ -1,1 +1,15 @@
 "use strict";
+// Problem - 8 : Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+{
+    const validateKeys = (obj, keys) => {
+        let res = true;
+        for (const key of keys) {
+            if (!(key in obj))
+                res = false;
+        }
+        return res;
+    };
+    const person = { name: 'Alice', age: 25, email: 'alice@example.com' };
+    console.log(validateKeys(person, ['name', 'age'])); // true
+    //   console.log(validateKeys(person, ['name', 'age', 'address'])) // false
+}
---
+++ problems/problem-8.ts
@@ -0,0 +1,17 @@
+// Problem - 8 : Create a function validateKeys(obj: T, keys: (keyof T)[]) that takes an object obj and an array of keys keys. The function should return true if all of the specified keys exist in the object; otherwise, it should return false.
+{
+  const validateKeys = <T extends object, Q extends (keyof T)[]>(
+    obj: T,
+    keys: Q
+  ): boolean => {
+    let res = true
+    for (const key of keys) {
+      if (!(key in obj)) res = false
+    }
+    return res
+  }
+
+  const person = { name: 'Alice', age: 25, email: 'alice@example.com' }
+  console.log(validateKeys(person, ['name', 'age'])) // true
+  //   console.log(validateKeys(person, ['name', 'age', 'address'])) // false
+}
